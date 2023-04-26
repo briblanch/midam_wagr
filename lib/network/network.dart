75c39ca9e26e4cad420ea82a68c466e70729a126
Brian Blanchard <briblanch@gmail.com>
2023-04-26 12:09:16 -0700

    Formatting

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
 
 Dio buildNetworkClient() {
-  return Dio(BaseOptions(
-      baseUrl: 'https://worldgolfranking2021api.wagr.com/api/wagr'));
+  return Dio(
+    BaseOptions(
+      baseUrl: 'https://worldgolfranking2021api.wagr.com/api/wagr',
+    ),
+  );
 }
